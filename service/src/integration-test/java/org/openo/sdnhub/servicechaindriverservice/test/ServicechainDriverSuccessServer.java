8575aa5e070699dd7ef4905e617c89680a2bb05f
subhash kumar singh <subhash.kumar.singh@huawei.com>
2017-02-08 20:45:36 +0530

    Health check for servicechain
    Health check for servicechain driver.
    
    Issue-Id: SDNHUB-47
    Change-Id: I5eb7d82d47b9aacb151c8f6c6ce7f625ecfbd280
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

---
+++ service/src/integration-test/java/org/openo/sdnhub/servicechaindriverservice/test/ServicechainDriverSuccessServer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2016 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.sdnhub.servicechaindriverservice.test;
+
+import org.openo.sdno.testframework.http.model.HttpRquestResponse;
+import org.openo.sdno.testframework.moco.MocoHttpServer;
+import org.openo.sdno.testframework.moco.responsehandler.MocoResponseHandler;
+
+public class ServicechainDriverSuccessServer extends MocoHttpServer {
+
+    public ServicechainDriverSuccessServer() {
+        super();
+    }
+
+    public ServicechainDriverSuccessServer(String configFile) {
+        // super(configFile);
+    }
+
+    @Override
+    public void addRequestResponsePairs() {
+
+       
+        this.addRequestResponsePair("src/integration-test/resources/openstack_mock_jsons/MSBGetServices.json",
+                new AcWanSuccessResponseHandler());
+    }
+
+    private class AcWanSuccessResponseHandler extends MocoResponseHandler {
+
+        @Override
+        public void processRequestandResponse(HttpRquestResponse httpObject) {
+
+        }
+    }
+
+}
